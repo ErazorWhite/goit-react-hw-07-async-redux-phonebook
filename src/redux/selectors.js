d8fdc39f604a9f767df58a01ca772ffbf77d614a
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-03-02 01:23:26 +0200

    selectors optimized

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filters.name;
-export const selectFilteredContacts = state => {
-  const contacts = selectContacts(state);
-  const filterValue = selectFilter(state)?.toLowerCase();
-  if (!contacts.length) return;
-  if (!filterValue) return contacts;
-  return contacts?.filter(({ name }) =>
-    name.toLowerCase().includes(filterValue)
-  );
-};
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter?.toLowerCase();
+    if (!contacts.length) return;
+    if (!normalizedFilter) return contacts;
+    return contacts?.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
